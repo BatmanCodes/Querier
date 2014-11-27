1751d5220b5848f21f0f9359601b36323c0708e3
kartikp1995 <kartikp1995@yahoo.co.in>
2014-11-27 23:20:10 +0530

    Design Left

--- learn.py
+++ learn.py
@@ -1,9 +1,10 @@
 import wx
 import wx.html
+import MySQLdb
 
-name = "Kartik"
-enroll_no = "No"
-img = "Photo.jpg"
+class NoEntryException(Exception):
+	pass
+
 class MyHtmlPanel(wx.Panel):
 	"""
 	class MyHtmlPanel inherits wx.Panel and adds a button and HtmlWindow
@@ -16,22 +17,78 @@ class MyHtmlPanel(wx.Panel):
 
 		self.tc = wx.TextCtrl(self, -1, "")
 		# .Add(tc, proportion=1)
+		self.error = wx.TextCtrl(self, -1, "", pos=(200,0), size=(200, 25), style=wx.TE_READONLY)
 
-		self.btn1 = wx.Button(self, -1, "Load", pos=(50,25))
+		self.btn1 = wx.Button(self, -1, "Search", pos=(50,25))
 		self.btn1.Bind(wx.EVT_BUTTON, self.OnLoadFile)
 
 		self.btn2 = wx.Button(self, -1, "Clear Page", pos=(120,0))
 		self.btn2.Bind(wx.EVT_BUTTON, self.OnClearPage)
+	
 	def OnLoadFile(self, event):
-		path = self.tc.GetValue()
-		Template = open('E:/Programs and Codes/Work/Querier/HTMLTemplate.html').read()
-		template = Template.replace("{{ enroll_no }}", path)
+		enroll_no = self.tc.GetValue()
+		print "Search: "+str(enroll_no)
+		try:	
+			vals = self.query(int(enroll_no))
+			self.error.SetValue("")
+		except NoEntryException as e:
+			self.error.SetValue("Invalid Enrollment No")
+			return
+		except ValueError:
+			self.error.SetValue("Invalid Enrollment No")
+			return
+
+		name = vals[2]
+		clas = vals[3]
+		branch = vals[4]
+		room_no = vals[6]
+		reg_entry = vals[7]
+		mobile_no = vals[8]
+		mess_reg = vals[9]
+		email = vals[10]
+		remark = vals[11]
+		image_path = '../'+vals[12]
+		fan = vals[13]
+		tube_light = vals[14]
+		study_table = vals[15]
+		tea_table = vals[16]
+		hard_bed = vals[17]
+		lan = vals[18]
+
+		Template = open('HTMLTemplate.html').read()
+		template = Template.replace("{{ enroll_no }}", enroll_no)
 		template = template.replace("{{ name }}", name)
-		template = template.replace("{{ img }}", img)
+		template = template.replace("{{ img }}", image_path)
+		template = template.replace("{{ class }}", clas)
+		template = template.replace("{{ branch }}", branch)
+		template = template.replace("{{ room_no }}", str(room_no))
+		template = template.replace("{{ reg_entry }}", reg_entry)
+		template = template.replace("{{ mobile_no }}", mobile_no)
+		template = template.replace("{{ mess_reg }}", mess_reg)
+		template = template.replace("{{ email }}", email)
+		template = template.replace("{{ remark }}", remark)
+		template = template.replace("{{ fan }}", fan)
+		template = template.replace("{{ tube_light }}", tube_light)
+		template = template.replace("{{ study_table }}", study_table)
+		template = template.replace("{{ tea_table }}", tea_table)
+		template = template.replace("{{ hard_bed }}", hard_bed)
+		template = template.replace("{{ lan }}", lan)
 		self.html1.SetPage(template)
 
 	def OnClearPage(self, event):
 		self.html1.SetPage("")
+
+	def query(self, enroll_no):
+		db = MySQLdb.connect(host="localhost", # your host, usually localhost
+		                     user="querier", # your username
+		                     passwd="querier", # your password
+		                     db="querier") # name of the data base
+		cur = db.cursor()
+		query = "SELECT * FROM main WHERE enroll_no = "+str(enroll_no)
+		cur.execute(query)
+		l = cur.fetchall()
+		return l[0]
+
 
 
 app = wx.PySimpleApp()
